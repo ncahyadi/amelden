d51b7723056540fdb90368146fa988908b9c040f
Abdul Aziz Anaoval <72790651+noval1802@users.noreply.github.com>
2025-12-11 02:35:42 +0700

    Implement 'salah' command in source.py
    Added a new command 'salah' to handle user requests.

--- source.py
+++ source.py
@@ -7,16 +7,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Gunakan /start untuk memulai bot.")
 
+async def salah(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Siap mohon izin kami pedomani dan laksanakan mohon izin.")
+
 async def amelden(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Selamat malam mohon ijin memperkenalkan diri dengan kami Letda Laut (E) Abdul Aziz Anaoval. S.Kom NRP 2225101020028400 PAPK 32  Alumni Universitas Pelita Bangsa Jurusan Teknik Informatika asal Bekasi selanjutnya mohon ijin arahan terimakasih mohon ijin")
 
 
 if __name__ == '__main__':
-    TOKEN = ''
+    TOKEN = '8519079354:AAEUNohS_GpCUjlIOn6JHyi02cw-N_F9IJM'
 
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("amelden", amelden))
+    app.add_handler(CommandHandler("salah", salah))
     app.run_polling()
